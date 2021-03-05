cf6ec841fbe796bc4a564919bf9c6f697a280461
Evan <60061381+E-W-Jones@users.noreply.github.com>
2021-03-05 00:37:35 +0000

    Update gravitational force/potential
    Instead of using np.divide with the where argument to avoid divide by 0 errors, just adding an identity matrix so its non-zero. Because it's not a physical circumstance it doesn't matter what the value is
    
    Still needs testing (but should run exactly the same)

--- solar_system.py
+++ solar_system.py
@@ -153,14 +153,15 @@ class SolarSystem():
 
         """
         mimj = self.mass_products
-        rij_mag = self.sep_mags
-        # Use np.divide with `where` to avoid dividing by 0.
+        rij_mag = self.sep_mags + np.eye(self.N)
+        # Adding the identity matrix to avoid division by 0.
         # Division by zero would happen for ri = rj,
         # which is when the ith and jth body are representing the same body.
-        # There is no potential or force felt on a body by itself, so return 0.
-        U_ij = -1 * G * np.divide(mimj, rij_mag, out=np.zeros_like(mimj),
-                                  where=(rij_mag != 0))
+        # There is no physical potential or force felt on a body by itself,
+        # it doesn't matter what the value is, so no harm in changing it to 1.
+        U_ij = -1 * G * mimj / rij_mag
         # An array where each element is the potential energy a body has
+        # Not a necessary step, but useful for troubleshooting
         U_i = np.sum(U_ij, axis=1)
         # The total gravitational potential of the system
         U_total = np.sum(U_i)
@@ -188,14 +189,17 @@ class SolarSystem():
         An array of the forces acting on the i-th particle; [F_1, F_2, ...]
 
         """
-        mimj = self.mass_products
-        rij_mag = self.sep_mags
+        # Add another dimension to scalars so can be broadcast with vectors
+        mimj = self.mass_products[:, :, np.newaxis]
+        rij_mag = (self.sep_mags + np.eye(self.N))[:, :, np.newaxis]
+
         rij = self.sep_vecs
-        # Use np.divide with `where` to avoid dividing by 0, as these are
-        # points where the two particles are the same, and there is no
-        # potential/force felt on a body by that body, so return 0 instead.
-        F_ij = G * np.divide(mimj, rij_mag**3, out=np.zeros_like(mimj),
-                             where=(rij_mag != 0))[:, :, np.newaxis] * rij
+        # Adding the identity matrix to avoid division by 0.
+        # Division by zero would happen for ri = rj,
+        # which is when the ith and jth body are representing the same body.
+        # There is no physical potential or force felt on a body by itself,
+        # it doesn't matter what the value is, sono harm in changing it to 1.
+        F_ij = G * mimj / rij_mag**3 * rij
         # An array where each element is the total force acting on a body
         F_i = np.sum(F_ij, axis=1)
         return F_i
